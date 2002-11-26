3802eb6a5416319fe360d8bf58dd261caeaa5879
garbers <garbers>
2002-11-26 23:13:39 +0000

    Handled some error cases

--- Applications/JGTaskEvaluator/jgcmdclient.sh
+++ Applications/JGTaskEvaluator/jgcmdclient.sh
@@ -16,7 +16,7 @@ mkdir -p $CLIENT_DIR
 CLIENT_NOTIFICATION_FILE=$CLIENT_DIR/finished
 echo "$@" > $CLIENT_DIR/cmd
 # read input (must be totally read). 
-cat > $CLIENT_DIR/stdin
+cat /dev/stdin > $CLIENT_DIR/stdin
 # create a notification fifo
 mkfifo $CLIENT_NOTIFICATION_FILE
 date >$CLIENT_DIR/scheduled
@@ -24,5 +24,14 @@ date >$CLIENT_DIR/scheduled
 echo "$CLIENT_DIR" >> $SERVER_NOTIFICATION_FILE
 # wait for notification
 read FINISHEDDATE <$CLIENT_NOTIFICATION_FILE
-wc $CLIENT_DIR/exitstatus $CLIENT_DIR/stdout $CLIENT_DIR/stderr
-cat $CLIENT_DIR/exitstatus $CLIENT_DIR/stdout $CLIENT_DIR/stderr
+
+if test "$JGCLIENTOUTPUT" == "all";
+then
+  wc $CLIENT_DIR/exitstatus $CLIENT_DIR/stdout $CLIENT_DIR/stderr
+  cat $CLIENT_DIR/exitstatus $CLIENT_DIR/stdout $CLIENT_DIR/stderr
+else 
+  cat $CLIENT_DIR/stdout >/dev/stdout
+  cat $CLIENT_DIR/stderr >/dev/stderr
+  read EXIT_CODE <$CLIENT_DIR/exitstatus
+  exit $EXIT_CODE
+fi
--- Applications/JGTaskEvaluator/jgcmdserver.sh
+++ Applications/JGTaskEvaluator/jgcmdserver.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 # global directory for several requests
-LOG_ROOT=$TMPDIR/$USER/jgcmdlog
+#LOG_ROOT=$TMPDIR/$USER/jgcmdlog
+LOG_ROOT=/User/$USER/tmp/jgcmdlog
+
 # this is a fifo, which is read by a server process with sufficient (port) access rights
 SERVER_NOTIFICATION_FILE=$LOG_ROOT/serverfifo
 rm -f $SERVER_NOTIFICATION_FILE
@@ -9,6 +11,7 @@ mkdir -p $LOG_ROOT
 mkfifo $SERVER_NOTIFICATION_FILE
 if test -e $SERVER_NOTIFICATION_FILE;
 then 
+  chmod go-rwx $SERVER_NOTIFICATION_FILE; # security
   echo $SERVER_NOTIFICATION_FILE;
 else
   exit 1;
@@ -17,17 +20,25 @@ fi
 echo $LOG_ROOT >~/.jgcmdlogroot
 
 # endless loop
-while read CLIENT_DIR <$SERVER_NOTIFICATION_FILE;
+while test -z "";
 do 
+  read CLIENT_DIR <$SERVER_NOTIFICATION_FILE
   # give chance to redraw schedule by setting forgetit
-  if test -e $CLIENT_DIR/forgetit
-  then
-    date > $CLIENT_DIR/forgot
+  if test -z "$CLIENT_DIR"; 
+  then echo "warning: read empty line" # this occurs both in bash and sh
+  elif test "$LAST_CLIENT_DIR" == "$CLIENT_DIR"
+  then echo "warning $CLIENT_DIR read twice" # this occurs often in bash, no time in sh.
+  elif ! test -d $CLIENT_DIR;
+  then echo "error (not a directory): $CLIENT_DIR"
+  elif test -e $CLIENT_DIR/forgetit
+  then date > $CLIENT_DIR/forgot
   else
-    read COMMAND <$CLIENT_DIR/cmd
-    echo $CLIENT_DIR: $COMMAND
-    date >$CLIENT_DIR/started
-    (($COMMAND) <$CLIENT_DIR/stdin >$CLIENT_DIR/stdout 2>$CLIENT_DIR/stderr ; echo $PIPESTATUS >$CLIENT_DIR/exitstatus; date >>$CLIENT_DIR/finished) &
+      read COMMAND <$CLIENT_DIR/cmd
+      echo "$CLIENT_DIR:$COMMAND"
+      date >$CLIENT_DIR/started
+      # $? is the exit status of the last foreground pipeline
+      (($COMMAND) <$CLIENT_DIR/stdin >$CLIENT_DIR/stdout 2>$CLIENT_DIR/stderr ; echo $? >$CLIENT_DIR/exitstatus; date >>$CLIENT_DIR/finished; echo "finished: $CLIENT_DIR:$COMMAND") &
+      LAST_CLIENT_DIR="$CLIENT_DIR"
   fi
 done
 
